a486718bf6e281a140b74c86861dcbceef4b76f9
_drazy <48094316+Deadboy666@users.noreply.github.com>
2026-04-28 21:57:28 -0500

    Add files via upload

---
+++ headcrab_native.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+
+    STEAM_CLIENT="$HOME/.steam/steam/client.sh"
+    INJECT_SLS="LD_AUDIT=$HOME/.local/share/SLSsteam/library-inject.so:$HOME/.local/share/SLSsteam/SLSsteam.so"
+
+       GameLauncher(){
+        export $INJECT_SLS &> /dev/null
+        source $STEAM_CLIENT "$@"
+        }
+
+
+
+    steam(){
+        GameLauncher "$@"
+        }
+
+   steam "$@"
